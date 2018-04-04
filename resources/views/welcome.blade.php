653057b3d9fe11cce7bfd0d1cbb207a154d3930c
MarcosBTG <marckgonzalezoficial@gmail.com>
2018-04-03 19:04:14 -0500

    nuevamente se cambio el home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Sistema de control de inventario</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,7 +79,10 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Sistema de control de inventarios.
+                </div>
+                <div class="title m-b-md">
+                    MarcosBTG
                 </div>
 
                 <div class="links">
