eb39b08b3c4475959a6987348c93ba58e013776b
MarcosBTG <marckgonzalezoficial@gmail.com>
2018-04-06 20:18:16 -0500

    Se agrego paquete de idioma Spanish

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Laraveles\Spanish\SpanishServiceProvider::class,
 
         /*
          * Application Service Providers...
